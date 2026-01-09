888ee8a69d81d5e5e930f6f22c16d4cb618edc63
Blake Bui <zsilverstonez@gmail.com>
2026-01-09 19:50:55 +1100

    Update admin_dashboard_account.php

--- admin_dashboard_account.php
+++ admin_dashboard_account.php
@@ -158,6 +158,15 @@ $connect->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sunny Spot Holidays - Admin Dashboard - Account</title>
     <link href="styles.css?v=<?php echo time(); ?>" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Arima:wght@100..700&family=Dancing+Script:wght@400..700&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Arima:wght@100..700&family=Dancing+Script:wght@400..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
     <style>
     .user-header {
         display: flex;
@@ -513,5 +522,6 @@ $connect->close();
         <img src="images/author.png" alt="author" class="author">
     </footer>
 </body>
+
 
 </html>
