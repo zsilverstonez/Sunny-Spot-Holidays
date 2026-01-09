096aa2c228f2123a84fa8d97c9ba1191129a7b82
Blake Bui <zsilverstonez@gmail.com>
2026-01-09 20:01:44 +1100

    Refactor reset password logic and improve validation

--- reset_password.php
+++ reset_password.php
@@ -77,7 +77,6 @@ $connect->close();
         href="https://fonts.googleapis.com/css2?family=Arima:wght@100..700&family=Dancing+Script:wght@400..700&display=swap"
         rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Arima:wght@100..700&family=Dancing+Script:wght@400..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
@@ -207,5 +206,6 @@ $connect->close();
     </footer>
     <script src="script.js"></script>
 </body>
+
 
 </html>
