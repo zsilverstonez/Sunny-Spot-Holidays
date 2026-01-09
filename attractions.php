efdb6e3fa4bb64948937284ce96529d75383e967
Blake Bui <zsilverstonez@gmail.com>
2026-01-09 19:57:32 +1100

    Fix HTML encoding issues in attractions.php

--- attractions.php
+++ attractions.php
@@ -15,7 +15,6 @@
         href="https://fonts.googleapis.com/css2?family=Arima:wght@100..700&family=Dancing+Script:wght@400..700&display=swap"
         rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Arima:wght@100..700&family=Dancing+Script:wght@400..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
@@ -328,5 +327,6 @@
     </footer>
     <script src="script.js"></script>
 </body>
+
 
 </html>
